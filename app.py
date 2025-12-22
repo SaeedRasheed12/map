b055cc56756ba0c9d736e1848fbbac1c777abe64
Saeed Rasheed <saeedrasheed225@gmail.com>
2025-12-22 17:40:00 +0500

    k"

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
-
+import os
 
 app = Flask(__name__)
-CORS(app)   # ✅ THIS LINE FIXES FLUTTER CONNECTION
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 # ---------------- ADMIN (HARDCODED) ----------------
@@ -24,32 +24,34 @@ class User(db.Model):
     password = db.Column(db.String(200))
 
 
+class Place(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+
+
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     floor = db.Column(db.String(50))
-    place_id = db.Column(db.Integer)
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id'))
 
 
 class Route(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    place_id = db.Column(db.Integer)
     from_location_id = db.Column(db.Integer)
     to_location_id = db.Column(db.Integer)
     floor = db.Column(db.String(50))
 
-class Place(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
 
 class RouteStep(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     route_id = db.Column(db.Integer)
     step_order = db.Column(db.Integer)
-    action = db.Column(db.String(50))   # walk, left, right, stairs_up
-    distance = db.Column(db.Float)      # meters
+    action = db.Column(db.String(50))
+    distance = db.Column(db.Float)
 
-
-# ---------------- AUTH APIs ----------------
+# ---------------- AUTH ----------------
 
 @app.route('/admin/login', methods=['POST'])
 def admin_login():
@@ -80,25 +82,45 @@ def user_login():
         return jsonify({"user_id": user.id})
     return jsonify({"error": "Invalid login"}), 401
 
+# ---------------- PLACES ----------------
+
+@app.route('/admin/place', methods=['POST'])
+def add_place():
+    data = request.json
+    place = Place(name=data['name'])
+    db.session.add(place)
+    db.session.commit()
+    return jsonify({"id": place.id, "name": place.name})
+
+
+@app.route('/places', methods=['GET'])
+def get_places():
+    return jsonify([
+        {"id": p.id, "name": p.name}
+        for p in Place.query.all()
+    ])
 
 # ---------------- LOCATIONS ----------------
 
 @app.route('/admin/location', methods=['POST'])
 def add_location():
     data = request.json
-    loc = Location(name=data['name'], floor=data['floor'])
+    loc = Location(
+        name=data['name'],
+        floor=data['floor'],
+        place_id=data['place_id']
+    )
     db.session.add(loc)
     db.session.commit()
     return jsonify({"message": "Location saved"})
 
 
-@app.route('/locations', methods=['GET'])
-def get_locations():
+@app.route('/locations/<int:place_id>', methods=['GET'])
+def get_locations_by_place(place_id):
     return jsonify([
         {"id": l.id, "name": l.name, "floor": l.floor}
-        for l in Location.query.all()
+        for l in Location.query.filter_by(place_id=place_id).all()
     ])
-
 
 # ---------------- ROUTE RECORDING ----------------
 
@@ -106,6 +128,7 @@ def get_locations():
 def start_route():
     data = request.json
     route = Route(
+        place_id=data['place_id'],
         from_location_id=data['from_id'],
         to_location_id=data['to_id'],
         floor=data['floor']
@@ -133,15 +156,16 @@ def save_route_step():
 def stop_route():
     return jsonify({"message": "Route completed"})
 
-
 # ---------------- USER NAVIGATION ----------------
 
 @app.route('/navigate', methods=['GET'])
 def navigate():
+    place_id = request.args.get('place_id')
     from_id = request.args.get('from')
     to_id = request.args.get('to')
 
     route = Route.query.filter_by(
+        place_id=place_id,
         from_location_id=from_id,
         to_location_id=to_id
     ).first()
@@ -158,24 +182,8 @@ def navigate():
         for s in steps
     ])
 
-@app.route('/admin/place', methods=['POST'])
-def add_place():
-    data = request.json
-    place = Place(name=data['name'])
-    db.session.add(place)
-    db.session.commit()
-    return jsonify({"place_id": place.id})
-
-@app.route('/locations/<int:place_id>', methods=['GET'])
-def get_locations_by_place(place_id):
-    return jsonify([
-        {"id": l.id, "name": l.name}
-        for l in Location.query.filter_by(place_id=place_id).all()
-    ])
-
-import os
-
 # ---------------- RUN ----------------
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
